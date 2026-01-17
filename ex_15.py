4e4fbe76e538ba1b2195312ce6187bd5b97bcb2d
Sagar <sagar@Sagars-MacBook-Air.local>
2026-01-17 18:14:25 +0530

    Initial commit

---
+++ ex_15.py
@@ -0,0 +1,36 @@
+class alien(object):
+    pass 
+
+class ship(object):
+    pass 
+
+class scene(object):
+    pass 
+
+class gothom(object):
+    pass 
+
+class escape_pod(scene):
+    pass 
+
+class planet(object):
+    pass 
+
+class map(object):
+    self.__init__(self):
+        pass 
+
+class engine(object):
+    pass 
+
+class death(scene):
+    pass 
+
+class central_corridor(scene):
+    pass 
+
+class laser_weapon_armory(scene):
+    pass 
+
+class bridge(scene):
+    pass 
